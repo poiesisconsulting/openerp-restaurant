7bb466f39ff06ce6b713271732035663ed76e41e
Marco <marco.alesan@gmail.com>
2014-07-15 15:43:04 -0400

    Authorizations: Split cancels authorization state

--- poi_restaurant/static/src/js/splitbill.js
+++ poi_restaurant/static/src/js/splitbill.js
@@ -199,10 +199,17 @@ function openerp_restaurant_splitbill(instance, module){
 
                     if(currentOrder.get('orderLines').models.length > 0){
                         //Authorizations: Remove auth. state first and then go with the split process
-                        (new instance.web.Model('pos.order')).get_func('sp_execute')(currentOrder.get_order_id(), 'back')
-                        .then(function(){
+                        console.log("MRC currentOrder.authorization", currentOrder.authorization);
+                        if (currentOrder.authorization.state != 'none'){
+                            if (confirm("Your authorization process will be lost. Continue?")){
+                                (new instance.web.Model('pos.order')).get_func('sp_execute')(currentOrder.get_order_id(), 'back')
+                                .then(function(){
+                                    self.pos_widget.screen_selector.set_current_screen('splitbill');
+                                });
+                            }
+                        } else {
                             self.pos_widget.screen_selector.set_current_screen('splitbill');
-                        });
+                        }
                     }
                 });
                 
