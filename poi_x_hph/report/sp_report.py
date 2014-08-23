756ae902cba0c5a4252cb0ee542718b58c1aa02c
Marco Sanchez <marco.sanchez@poiesisconsulting.com>
2014-08-22 21:09:17 -0400

    S&P fixed amounts comparing with Daily Report

--- poi_x_hph/report/sp_report.py
+++ poi_x_hph/report/sp_report.py
@@ -24,6 +24,8 @@ import time
 from openerp.osv import osv
 from openerp.report import report_sxw
 
+TOTALS = {}
+
 
 class sp_report_functions(report_sxw.rml_parse):
     _name = 'report.account.account.balance'
@@ -51,7 +53,26 @@ class sp_report_functions(report_sxw.rml_parse):
 
             'get_sp_orders': self._get_sp_orders,
             'get_total_order': self._get_total_order,
+
         })
+
+        global TOTALS
+        TOTALS['collected'] = 0.00
+        TOTALS['checks'] = 0.00
+        TOTALS['covers'] = 0.00
+        TOTALS['sp_amount'] = 0.00
+        TOTALS['all_products'] = 0.00
+        TOTALS['tax'] = 0.00
+        TOTALS['gratuities'] = 0.00
+        TOTALS['food'] = 0.00
+        TOTALS['liquor'] = 0.00
+        TOTALS['cocktails'] = 0.00
+        TOTALS['wine'] = 0.00
+        TOTALS['beer'] = 0.00
+        TOTALS['non_alcoholic'] = 0.00
+        TOTALS['beverage'] = 0.00
+        TOTALS['misc'] = 0.00
+
         self.context = context
 
     def set_context(self, objects, data, ids, report_type=None):
@@ -62,12 +83,13 @@ class sp_report_functions(report_sxw.rml_parse):
         return super(sp_report_functions, self).set_context(objects, data, new_ids, report_type=report_type)
 
     def _get_sp_orders(self, form):
+        global TOTALS
         start_date = form['start_date'] + ' 00:00:00'
         end_date = form['end_date'] + ' 23:59:59'
         sp_journal_id = self.pool.get("pos.order").fetch_sp_journal_id(self.cr, self.uid)  # at Bacchanal --> 12
 
         self.cr.execute("""
-            SELECT DISTINCT ord.id as "id",
+            SELECT ord.id as "id",
                 date(ses.start_at) as "session_date",
                 ord.name as "order_name",
                 ord.covers as "covers",
@@ -104,6 +126,8 @@ class sp_report_functions(report_sxw.rml_parse):
 
         orders = self.cr.dictfetchall()
         for order in orders:
+            TOTALS['checks'] += 1
+            TOTALS['covers'] += order['covers']
             tot_order = self._get_total_order(order['id'])
 
             order['tot_no_tax'] = tot_order['tot_no_tax']
@@ -114,13 +138,6 @@ class sp_report_functions(report_sxw.rml_parse):
             else:
                 order['sp_percentage'] = "%.2f" % ((order['sp_amount'] / tot_order['tot_w_tax']) * 100)
 
-            # if tot_order['tot_no_tax'] < 0:
-            #     orders.remove(order)
-            # else:
-            #     order['tot_no_tax'] = tot_order['tot_no_tax']
-            #     order['tot_w_tax'] = tot_order['tot_w_tax']
-            #     order['n_lines'] = tot_order['n_lines']
-            #     order['sp_percentage'] = "%.2f" % ((order['sp_amount'] / tot_order['tot_w_tax']) * 100)
         return orders
 
     def _get_total_order(self, order_id):
@@ -175,117 +192,109 @@ class sp_report_functions(report_sxw.rml_parse):
                 return True
         return False
 
-    def _get_total(self, form, filter):
-        total = 0
+    def _get_totals(self, form):
+        global TOTALS
 
         for order in self._get_sp_orders(form):
+            TOTALS['collected'] += order['sp_amount']
             for line in self._get_lines_in_order(order['id']):
                 product = self._get_product_in_line(line.product_id.id)
-                percentage = float(order['sp_percentage']) / 100
-                amount_wo_tax = line.price_subtotal * percentage
-                amount_with_tax = line.price_subtotal_incl * percentage
+                TOTALS['all_products'] += 1
+                TOTALS['tax'] += line.price_subtotal_incl - line.price_subtotal
+                if product[1].name.upper() == 'GRATUITY':
+                    TOTALS['gratuities'] += line.price_subtotal
+                if self._has_category(product, 'liquor'):
+                    TOTALS['liquor'] += 1
+                if self._has_category(product, 'cocktails'):
+                    TOTALS['cocktails'] += 1
+                if self._has_category(product, 'wine'):
+                    TOTALS['wine'] += 1
+                if self._has_category(product, 'beer'):
+                    TOTALS['beer'] += 1
+                if self._has_category(product, 'non alcoholic'):
+                    TOTALS['non_alcoholic'] += 1
+                if self._has_category(product, 'misc'):
+                    TOTALS['misc'] += 1
+        TOTALS['sp_amount'] = TOTALS['collected'] - TOTALS['tax'] - TOTALS['gratuities']
+        TOTALS['beverage'] = TOTALS['liquor'] + TOTALS['cocktails'] + TOTALS['wine'] + TOTALS['beer'] + TOTALS['non_alcoholic']
+        TOTALS['food'] = TOTALS['all_products'] - TOTALS['misc'] - TOTALS['beverage']
+        return TOTALS
 
-                if filter == 'tax':
-                    total += amount_with_tax - amount_wo_tax
-
-                elif filter == 'gratuities' and product[1].name.upper() == 'GRATUITY':
-                        total += amount_wo_tax
-
-                elif self._has_category(product, filter):  # filter must be a category name
-                        total += amount_wo_tax
-
-        return total
-
-    def _get_sp_total(self, form):
-        food = self._get_total(form, "food")
-        beverage = self._get_total(form, "beverage")
-        misc = self._get_total(form, "misc")
-
-        return food + beverage + misc
     ###########################################################
     ###### local context functions ############################
     # name of category can be upper-lower case (nevermind)
     # "%.2f" % total >> always shows 'total' with two decimals
 
     def _get_total_food_sp(self, form):
-        total = self._get_total(form, "food")
-        return "%.2f" % total
+        self._get_totals(form)
+        global TOTALS
+        return "%.2f" % (float(TOTALS['sp_amount']) * (float(TOTALS['food']) / float(TOTALS['all_products'])))
 
     def _get_total_liquor_sp(self, form):
-        total = self._get_total(form, "liquor")
-        return "%.2f" % total
+        global TOTALS
+        return "%.2f" % (float(TOTALS['sp_amount']) * (float(TOTALS['liquor']) / float(TOTALS['all_products'])))
 
     def _get_total_cocktail_sp(self, form):
-        total = self._get_total(form, "cocktails")
-        return "%.2f" % total
+        global TOTALS
+        return "%.2f" % (float(TOTALS['sp_amount']) * (float(TOTALS['cocktails']) / float(TOTALS['all_products'])))
 
     def _get_total_wine_sp(self, form):
-        total = self._get_total(form, "wine")
-        return "%.2f" % total
+        global TOTALS
+        return "%.2f" % (float(TOTALS['sp_amount']) * (float(TOTALS['wine']) / float(TOTALS['all_products'])))
 
     def _get_total_beer_sp(self, form):
-        total = self._get_total(form, "beer")
-        return "%.2f" % total
+        global TOTALS
+        return "%.2f" % (float(TOTALS['sp_amount']) * (float(TOTALS['beer']) / float(TOTALS['all_products'])))
 
     def _get_total_nonalcoholic_sp(self, form):
-        total = self._get_total(form, "non alcoholic")
-        return "%.2f" % total
+        global TOTALS
+        return "%.2f" % (float(TOTALS['sp_amount']) * (float(TOTALS['non_alcoholic']) / float(TOTALS['all_products'])))
 
     def _get_total_beverage_sp(self, form):
-        total = self._get_total(form, "beverage")
-        return "%.2f" % total
+        global TOTALS
+        return "%.2f" % (float(TOTALS['sp_amount']) * (float(TOTALS['beverage']) / float(TOTALS['all_products'])))
 
     def _get_total_misc_sp(self, form):
-        total = self._get_total(form, "misc")
-        return "%.2f" % total
+        global TOTALS
+        return "%.2f" % (float(TOTALS['sp_amount']) * (float(TOTALS['misc']) / float(TOTALS['all_products'])))
 
     def _get_total_sales_sp(self, form):
-        total = self._get_sp_total(form)
-        return "%.2f" % total
+        global TOTALS
+        return "%.2f" % TOTALS['sp_amount']
 
     def _get_total_taxes(self, form):
-        total = self._get_total(form, "tax")
-        return "%.2f" % total
+        global TOTALS
+        return "%.2f" % TOTALS['tax']
 
     def _get_total_gratuities(self, form):
-        total = self._get_total(form, "gratuities")
-        return "%.2f" % total
+        global TOTALS
+        return "%.2f" % TOTALS['gratuities']
 
     def _get_total_collected(self, form):
-        sp_tot = self._get_sp_total(form)
-        tax = self._get_total(form, "tax")
-        gratuities = self._get_total(form, "gratuities")
-        return "%.2f" % (sp_tot + tax + gratuities)
+        global TOTALS
+        return "%.2f" % (TOTALS['collected'])
 
     def _get_n_of_checks(self, form):
-        sp_orders = self._get_sp_orders(form)
-        return "%.2f" % sp_orders.__len__()
+        global TOTALS
+        return "%.2f" % TOTALS['checks']
 
     def _get_n_of_covers(self, form):
-        sp_orders = self._get_sp_orders(form)
-        tot_covers = 0
-        for order in sp_orders:
-            tot_covers += order['covers']
-        return "%.2f" % tot_covers
+        global TOTALS
+        return "%.2f" % TOTALS['covers']
 
     def _get_average_check(self, form):
-        sp_tot = self._get_sp_total(form)
-        sp_orders = self._get_sp_orders(form)
+        global TOTALS
 
-        if sp_orders.__len__() > 0:
-            return "%.2f" % (sp_tot / sp_orders.__len__())
+        if TOTALS['checks'] > 0:
+            return "%.2f" % (TOTALS['sp_amount'] / TOTALS['checks'])
         else:
             return "%.2f" % 0.00
 
     def _get_average_cover(self, form):
-        sp_tot = self._get_sp_total(form)
-        tot_covers = 0
-        sp_orders = self._get_sp_orders(form)
-        for order in sp_orders:
-            tot_covers += order['covers']
+        global TOTALS
 
-        if tot_covers > 0:
-            return "%.2f" % (sp_tot / tot_covers)
+        if TOTALS['checks'] > 0:
+            return "%.2f" % (TOTALS['sp_amount'] / TOTALS['covers'])
         else:
             return "%.2f" % 0.00
 
